637eb2cae640db3ff7b32277af620743c1987fb6
Tommy Liao <tommyliao340@hotmail.com>
2019-08-26 14:30:37 -0700

    [ops] Sets up projects folder

---
+++ frontend/components/Projects/projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <style jsx>
+        {`
+          .projects {
+            height: 800px;
+            width: 100%;
+            background: #f5ab4e;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Projects;
