6e8ca22b26593226391b166a1515590157591ba4
Tommy Liao <tommyliao340@hotmail.com>
2021-02-17 14:00:28 -0800

    [prog] Adds inset

---
+++ portfolio-svelte/src/Splash/components/inset.svelte
@@ -0,0 +1,111 @@
+<script>
+  import Icons from "./icons.svelte";
+</script>
+
+<div class="inset">
+  <img
+    src="https://dudemanppl.s3-us-west-1.amazonaws.com/41962026_1898602030162209_5312635139379953664_o+(1).jpg"
+    alt="Me at Joshua Tree National Park at sunset"
+  />
+  <div class="name">Tommy Liao</div>
+  <div class="bio">Full Stack Developer</div>
+  <Icons />
+  <a
+    class="resume"
+    href="https://dudemanppl.s3-us-west-1.amazonaws.com/resume.pdf"
+  >
+    Resume
+  </a>
+</div>
+
+<style>
+  img {
+    height: 250px;
+    width: 250px;
+    border-radius: 50%;
+    border: solid white 5px;
+    box-shadow: 0px 0px 15px rgb(200, 200, 200);
+  }
+  .resume {
+    display: none;
+    font-size: 25px;
+    font-weight: bold;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  .name {
+    font-weight: bold;
+    font-style: italic;
+    font-size: 60px;
+  }
+  .bio {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  .inset {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    top: 50px;
+    height: 500px;
+    width: 500px;
+    background: rgb(250, 250, 250);
+    box-shadow: 20px 20px 30px rgb(11, 104, 133);
+  }
+  @media all and (max-width: 700px) {
+    .resume {
+      margin-top: 10px;
+      display: flex;
+    }
+
+    .inset {
+      width: 100%;
+      height: 80%;
+      box-shadow: 0 20px 30px rgb(11, 104, 133);
+    }
+  }
+
+  @media all and (max-width: 400px) {
+    .name {
+      font-size: 40px;
+    }
+  }
+
+  @media all and (max-height: 585px) {
+    img {
+      height: 220px;
+      width: 220px;
+    }
+    .name {
+      font-size: 35px;
+    }
+    .bio {
+      font-size: 18px;
+    }
+    .resume {
+      font-size: 20px;
+    }
+  }
+
+  @media all and (max-height: 510px) {
+    img {
+      height: 200px;
+      width: 200px;
+    }
+    .name {
+      font-size: 30px;
+    }
+    .bio {
+      font-size: 15px;
+    }
+    .resume {
+      font-size: 18px;
+    }
+  }
+</style>
--- portfolio-svelte/src/Splash/index.svelte
+++ portfolio-svelte/src/Splash/index.svelte
@@ -1,10 +1,10 @@
 <script>
-  import Icons from "./components/icons.svelte";
+  import Inset from "./components/inset.svelte";
 </script>
 
 <div class="splash">
-  <Icons />
-  <slot />
+  <Inset />
+  <div class="arrow" />
 </div>
 
 <style>
@@ -27,5 +27,71 @@
       rgb(63, 168, 200) 90%
     );
     height: calc(100vh - 60px);
+  }
+
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0) rotate(135deg);
+    }
+    40% {
+      transform: translateY(-30px) rotate(135deg);
+    }
+    60% {
+      transform: translateY(-15px) rotate(135deg);
+    }
+  }
+
+  .arrow {
+    position: absolute;
+    bottom: 30px;
+    width: 35px;
+    height: 35px;
+    border-top: 4px solid white;
+    border-right: 4px solid white;
+    animation: bounce 2s infinite;
+  }
+
+  @media all and (max-width: 700px) {
+    .splash {
+      top: 0;
+      height: 100vh;
+    }
+  }
+
+  @media all and (max-width: 700px), (max-height: 750px) {
+    .arrow {
+      width: 25px;
+      height: 25px;
+      border-top: 3px solid white;
+      border-right: 3px solid white;
+    }
+
+    @keyframes bounce {
+      0%,
+      20%,
+      50%,
+      80%,
+      100% {
+        transform: translateY(0) rotate(135deg);
+      }
+      40% {
+        transform: translateY(-20px) rotate(135deg);
+      }
+      60% {
+        transform: translateY(-15px) rotate(135deg);
+      }
+    }
+  }
+
+  @media all and (max-height: 600px) {
+    .arrow {
+      bottom: 10px;
+      width: 20px;
+      height: 20px;
+    }
   }
 </style>
