9e0bd0850ecd55d96758ae744ec8eb4332afbbc8
Tommy Liao <tommyliao340@hotmail.com>
2021-02-23 12:15:29 -0800

    [prog] Imma assume its better practice to use built in svelte store instead of jumbled other stuff

--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { writable, readable, get } from "svelte/store";
 
 const createCurrentSection = () => {
   const { subscribe, set } = writable("Top");
@@ -8,14 +8,74 @@ const createCurrentSection = () => {
   return { subscribe, setCurrentSection };
 };
 
-const sections = {};
-
-const addSection = (sectionName, element) => {
-  sections[sectionName] = element;
-};
-
-Object.setPrototypeOf(sections, { ...Object.prototype, addSection });
-
 const currentSection = createCurrentSection();
 
-export { currentSection, sections };
+const createSections = () => {
+  const { subscribe, update } = writable({});
+
+  const updateSections = (newSection) => {
+    update((sections) => {
+      return { ...sections, ...newSection };
+    });
+  };
+
+  return { subscribe, updateSections };
+};
+
+const sections = createSections();
+
+const createSectionIntersectionRatios = () => {
+  const { subscribe, update } = writable({});
+
+  return { subscribe };
+};
+
+const sectionIntersectionRatios = createSectionIntersectionRatios();
+
+const observerOptions = {
+  threshold: [
+    0,
+    0.01,
+    0.02,
+    0.03,
+    0.04,
+    0.05,
+    0.06,
+    0.07,
+    0.08,
+    0.09,
+    0.1,
+    0.11,
+    0.12,
+    0.13,
+    0.14,
+    0.15,
+    0.16,
+    0.17,
+    0.18,
+    0.19,
+  ],
+  // threshold: 0.15,
+};
+
+const createObserver = () => {
+  const handleIntersect = ([
+    {
+      intersectionRatio,
+      target: {
+        dataset: { section },
+      },
+    },
+  ]) => {
+    // console.log(section, intersectionRatio);
+  };
+
+  const { subscribe } = readable(
+    new IntersectionObserver(handleIntersect, observerOptions)
+  );
+  return { subscribe };
+};
+
+const observer = createObserver();
+
+export { currentSection, sections, sectionIntersectionRatios, observer };
--- src/util/intersectionObserver.svelte
+++ src/util/intersectionObserver.svelte
@@ -1,55 +1,21 @@
 <script>
   import { onMount } from "svelte";
-  import { currentSection, sections } from "../stores.js";
-  const { setCurrentSection } = currentSection;
-  const { addSection } = sections;
+  import { sections, observer } from "../stores.js";
+  const { updateSections } = sections;
 
   export let sectionName;
-  export let cb = () => {};
 
-  const sectionIntersectionRatios = {};
-  let observer;
   let childElement;
 
-  const handleIntersect = (entries, observer) => {
-    const [{ intersectionRatio }] = entries;
-
-    if (intersectionRatio > 0 && intersectionRatio < 1) {
-      console.log(sectionName, entries[0].intersectionRatio);
-
-      sectionIntersectionRatios[sectionName] = intersectionRatio;
-      console.log(sectionIntersectionRatios);
-      // console.table({ rootBounds, boundingClientRect, intersectionRect });
-      // console.log(target);
-      setCurrentSection(sectionName);
-      // $sections[$currentSection].scrollIntoView({
-      //   behavior: "smooth",
-      // });
-      cb(entries, observer);
-    }
-  };
-
-  const observerOptions = {
-    threshold: (() => {
-      const thresholdArr = [];
-
-      for (let i = 0; i < 20; i += 1) {
-        thresholdArr.push(i / 100);
-      }
-      return thresholdArr;
-    })(),
-  };
-
   onMount(() => {
-    observer = new IntersectionObserver(handleIntersect, observerOptions);
-    observer.observe(childElement);
-    addSection(sectionName, childElement);
+    $observer.observe(childElement);
+    updateSections({ [sectionName]: childElement });
 
-    return () => observer.unobserve(childElement);
+    return () => $observer.unobserve(childElement);
   });
 </script>
 
-<div bind:this={childElement}>
+<div bind:this={childElement} data-section={sectionName}>
   <slot />
 </div>
 
