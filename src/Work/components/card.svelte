079bdab4d5510264491fadf2c2772ad0f6e87aea
Tommy Liao <tommyliao340@hotmail.com>
2021-03-31 12:26:56 -0700

    [prog] First commit

---
+++ src/Work/components/card.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let project;
+  const {
+    name,
+    techStack,
+    description,
+    bulletPoints,
+    media,
+    backgroundColor,
+    githubLink,
+  } = project;
+</script>
+
+<div class="card" style="--background-color: rgb({backgroundColor})">
+  {name}
+</div>
+
+<style>
+  div {
+    width: 35vw;
+    height: 40vh;
+    margin: 20px;
+    background-color: var(--background-color);
+  }
+</style>
--- src/Work/index.svelte
+++ src/Work/index.svelte
@@ -1,16 +1,27 @@
 <script>
   import Intersection from "../util/intersectionObserver.svelte";
+  import Card from "./components/card.svelte";
+  import projects from "../../dummy";
 </script>
 
 <Intersection sectionName="work">
-  <div class="test">nice</div>
+  <div class="work">
+    {#each projects as project}
+      <Card {project} />
+    {/each}
+  </div>
 </Intersection>
 
 <style>
-  .test {
+  .work {
+    position: relative;
     z-index: 1;
     width: 100%;
     height: 100vh;
-    /* background: grey; */
+    display: flex;
+    /* padding: 40px; */
+    justify-content: center;
+    /* background: white; */
+    align-items: center;
   }
 </style>
